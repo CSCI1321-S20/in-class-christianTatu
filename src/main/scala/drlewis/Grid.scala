c6003ee3fa0485c89d22090289631efef8d9f919
Christian Tatu <Christian Tatu>
2020-02-04 10:13:17 -0600

    lewis 1st day in-class

---
+++ src/main/scala/drlewis/Grid.scala
@@ -0,0 +1,9 @@
+package drlewis
+
+import scalafx.scene.canvas.GraphicsContext
+
+class grid {
+    def render(gc: GraphicsContext): Unit = ???
+    def update()
+
+}
---
+++ src/main/scala/drlewis/PillHalf.scala
@@ -0,0 +1,5 @@
+package drlewis
+
+class PillHalf(val x: Int, y: Int, val color: ColorOption.Value) extends GridCell {
+    def draw(gc: GraphicsContext): Unit = ???
+}
